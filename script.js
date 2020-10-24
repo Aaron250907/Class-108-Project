3ceb529485f897662ba2d33546aac1f634153343
Aaron250907 <73229815+Aaron250907@users.noreply.github.com>
2020-10-24 15:34:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+  });
+
+  camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+      
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+  console.log('ml5 version:', ml5.version);
+  
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/E1bs1CXQq/model.json',modelLoaded);
+function modelLoaded() {
+  console.log('Model Loaded!');
+}
+    
+function check()
+{
+  img = document.getElementById('captured_image');
+  classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+if (error) {
+  console.error(error);
+} else {
+  console.log(results);
+  
+  document.getElementById("result_object_name").innerHTML = results[0].label;
+
+  gesture = results[0].label;
+  toSpeak = "";
+  
+  if(gesture == "Peace")
+  {
+    toSpeak = "Peace";
+    document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;";
+  }
+  else if(gesture == "Amazing")
+  {
+    toSpeak = "Amazing";
+    document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;";
+  }
+  else if(gesture == "Wonderful")
+  {
+    toSpeak = "Wonderful";
+    document.getElementById("result_object_gesture_icon").innerHTML = "&#128077;";
+  }
+  else if(gesture == "Yo")
+  {
+    toSpeak = "Yo";
+    document.getElementById("result_object_gesture_icon").innerHTML = "&#129304;";
+  }
+  else if(gesture == "Hello")
+  {
+    toSpeak = "Hello";
+    document.getElementById("result_object_gesture_icon").innerHTML = "&#128075;";
+  }
+  else if(gesture == "Not Good")
+  {
+    toSpeak = "Not Goood";
+    document.getElementById("result_object_gesture_icon").innerHTML = "&#128078;";
+  }
+  else if(gesture == "Hope")
+  {
+    toSpeak = "Let's Hope";
+    document.getElementById("result_object_gesture_icon").innerHTML = "&#9996;";
+  }
+  speak();
+}
+}
+
+
+function speak(){
+  var synth = window.speechSynthesis;
+
+  speak_data = toSpeak;
+
+  var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+  synth.speak(utterThis);
+
+}
